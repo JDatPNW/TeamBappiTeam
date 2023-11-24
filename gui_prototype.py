5d3f440d7ac7a84501da0830dfc77c67861ffa34
David <david.richter@freenet.de>
2023-11-24 22:18:49 +0900

    added save functionality

--- gui_prototype.py
+++ gui_prototype.py
@@ -47,6 +47,13 @@ def load_file():
     my_image = ctk.CTkImage(dark_image=PIL_Image, size=(666, 666)) # Creating a CTkImage object from the PIL image
     label.configure(image=my_image, text="") # Configuring the label to display the loaded image
 
+def save_file():
+    global my_image, label, PIL_Image  # Declaring global variables
+    filename = ctk.filedialog.asksaveasfile(mode='w', defaultextension=".jpeg")
+    if not filename:
+        return
+    my_image._dark_image.convert('RGB').save(filename)
+
 def run_model(): # TODO: implement model once it exists
     """
     The function `run_model()` converts the loaded image to grayscale,
@@ -57,23 +64,26 @@ def run_model(): # TODO: implement model once it exists
     image = my_image._dark_image  # Accessing the dark image from the CTkImage object
     grey_image = image.convert('LA')  # Converting the image to grayscale
     grey_image_ctk = ctk.CTkImage(dark_image=grey_image, size=(666, 666))  # Creating a CTkImage object from the grayscale image
-    label.configure(image=grey_image_ctk, text="")  # Configuring the label to display the grayscale image
+    my_image = grey_image_ctk
+    label.configure(image=my_image, text="")  # Configuring the label to display the grayscale image
 
 """
 This code is creating a GUI (Graphical User Interface) for an image viewer application using the
 customtkinter module.
 """
 label = ctk.CTkLabel(root, width=666, height=666, text="Select Image")  # Creating a label widget for displaying images
-label.grid(row=1, column=0, columnspan=3)  # Placing the label in the grid layout
+label.grid(row=1, column=0, columnspan=4)  # Placing the label in the grid layout
 
 # Creating buttons for loading, exiting, and running the model
 button_load = ctk.CTkButton(root, command=load_file, text="Load", fg_color="#2B719E", hover_color="#1B4864") # Load, Blue
+button_save = ctk.CTkButton(root, command=save_file, text="Save", fg_color="#2B719E", hover_color="#1B4864") # Load, Blue
 button_exit = ctk.CTkButton(root, text="Exit", fg_color="#9E2B2B", hover_color="#641B1B", command=root.quit) # Exit, Red
 button_run = ctk.CTkButton(root, text="Run", command=run_model, fg_color="#239E8B", hover_color="#10473E") # Run, Green
 
 # Placing the buttons in the grid layout
-button_load.grid(row=5, column=0, pady=3) # Left
-button_exit.grid(row=5, column=1, pady=3) # Middle
-button_run.grid(row=5, column=2, pady=3) # Right
+button_exit.grid(row=5, column=0, pady=3) # Left
+button_load.grid(row=5, column=1, pady=3) # Center Left
+button_save.grid(row=5, column=2, pady=3) # Center Right
+button_run.grid(row=5, column=3, pady=3) # Right
 
 root.mainloop()  # Running the main event loop to display the GUI
