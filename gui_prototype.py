7b264894105adf2e6d65a111163dc21484f94bca
JD <JDatPNW@users.noreply.github.com>
2023-11-25 14:20:58 +0900

    added status message label and error proofing, more comments

--- gui_prototype.py
+++ gui_prototype.py
@@ -22,7 +22,7 @@ Viewer". Finally, it sets the size of the main window to 666x700 pixels.
 ctk.set_default_color_theme("dark-blue")  # Set the default color theme for customtkinter: "dark-blue"
 root = ctk.CTk()  # Creating a custom tkinter instance
 root.title("Image Viewer")  # Setting the title of the main window to "Image Viewer"
-root.geometry("666x700")  # Setting the size of the main window to 666x700 pixels
+root.geometry("666x733")  # Setting the size of the main window to 666x700 pixels
 
 def load_file():
     """
@@ -30,29 +30,71 @@ def load_file():
     file using PIL, creates a CTkImage object from the PIL image, and configures a label to display the
     loaded image.
     """
-    global my_image, label, PIL_Image  # Declaring global variables
+    global my_image, label, status, PIL_Image  # Declaring global variables
     # Defining a tuple of file types for the file dialog
     filetypes = (
-        ('text files', '*.png'),
-        ('text files', '*.jpg'),
-        ('All files', '*.*')
+        ('image files', '*.png'),
+        ('image files', '*.jpg'),
+        ('image files', '*.jpeg'),
     )
-    # Displaying the file dialog to select a file
+
+    status.configure(text="Select Image from Dialog Window") # Status Message
+
+    # Displaying the file dialog to select the image file
     filename = ctk.filedialog.askopenfilename(
-        title='Open a file',
+        title='Open an image',
         initialdir='/',
         filetypes=filetypes
     )
+    if not filename: # If no file was chosen, exit the function without opening anything
+        status.configure(text="No Image selected - Try again!") # Status Message
+        return
+
+    status.configure(text="Image Loading...") # Status Message
+
     PIL_Image = Image.open(filename) # Opening the selected image file using PIL
     my_image = ctk.CTkImage(dark_image=PIL_Image, size=(666, 666)) # Creating a CTkImage object from the PIL image
     label.configure(image=my_image, text="") # Configuring the label to display the loaded image
+    status.configure(text="Image Loaded! You can now run the model!") # Status Message
 
 def save_file():
-    global my_image, label, PIL_Image  # Declaring global variables
-    filename = ctk.filedialog.asksaveasfile(mode='w', defaultextension=".jpeg")
-    if not filename:
+    """
+    The function "save_file" saves an image as a JPEG file.
+    """
+    global my_image, label, status, PIL_Image  # Declaring global variables
+
+    try: # check if image was already loaded
+        my_image
+    except NameError: # if not abort
+        status.configure(text="No Image loaded yet! - Load one before saving!") # Status Message    
         return
+
+    # Defining a tuple of file types for the file dialog
+    filetypes = (
+        ('image files', '*.jpeg'),
+        ('image files', '*.jpg')
+    )
+
+    status.configure(text="Save Image via Dialog Window") # Status Message
+
+    # Displaying the file dialog to save the image
+    filename = ctk.filedialog.asksaveasfile(
+        mode='w',
+        defaultextension=".jpeg",
+        title='Save the image',
+        initialdir='/',
+        filetypes=filetypes
+)
+
+    if not filename: # If no file was chosen, exit the function without opening anything
+        status.configure(text="Failed Saving Image - Make sure filename location are correct and try again!") # Status Message
+        return
+    
+    status.configure(text="Image Saving...") # Status Message
+
     my_image._dark_image.convert('RGB').save(filename)
+    status.configure(text="Image Saved!") # Status Message
+
 
 def run_model(): # TODO: implement model once it exists
     """
@@ -60,12 +102,22 @@ def run_model(): # TODO: implement model once it exists
     creates a CTkImage object from the grayscale image, and updates the
     label to display the grayscale version.
     """
-    global my_image, label, PIL_Image  # Declaring global variables
+    global my_image, label, status, PIL_Image  # Declaring global variables
+
+    try: # check if image was already loaded
+        my_image
+    except NameError: # if not abort
+        status.configure(text="No Image loaded yet! - Load one before running!")     # Status Message
+        return
+
+    status.configure(text="Image converting...") # Status Message   
     image = my_image._dark_image  # Accessing the dark image from the CTkImage object
     grey_image = image.convert('LA')  # Converting the image to grayscale
     grey_image_ctk = ctk.CTkImage(dark_image=grey_image, size=(666, 666))  # Creating a CTkImage object from the grayscale image
     my_image = grey_image_ctk
     label.configure(image=my_image, text="")  # Configuring the label to display the grayscale image
+    status.configure(text="Image converted!") # Status Message  
+
 
 """
 This code is creating a GUI (Graphical User Interface) for an image viewer application using the
@@ -73,6 +125,9 @@ customtkinter module.
 """
 label = ctk.CTkLabel(root, width=666, height=666, text="Select Image")  # Creating a label widget for displaying images
 label.grid(row=1, column=0, columnspan=4)  # Placing the label in the grid layout
+
+status = ctk.CTkLabel(root, text="Select Image")  # Creating a label widget for displaying status messages
+status.grid(row=2, column=0, columnspan=4)  # Placing the status message label in the grid layout
 
 # Creating buttons for loading, exiting, and running the model
 button_load = ctk.CTkButton(root, command=load_file, text="Load", fg_color="#2B719E", hover_color="#1B4864") # Load, Blue
